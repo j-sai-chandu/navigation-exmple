ecff55f451c7068d3fadfdc4f849bfa9f21c9b88
Lyove <lyove@outlook.com>
2024-03-05 11:55:14 +0800

    init

--- resources/views/backend/settings/fields/textarea.blade.php
+++ resources/views/backend/settings/fields/textarea.blade.php
@@ -5,16 +5,24 @@ $required_mark = ($required != "") ? '<span class="text-danger"> <strong>*</stro
 
 <div class="form-group mb-3 {{ $errors->has($field['name']) ? ' has-error' : '' }}">
     <label for="{{ $field['name'] }}" class='form-label'> <strong>{{ __($field['label']) }}</strong> ({{ $field['name'] }})</label> {!! $required_mark !!}
-    <textarea type="{{ $field['type'] }}" name="{{ $field['name'] }}" class="form-control {{ Arr::get( $field, 'class') }} {{ $errors->has($field['name']) ? ' is-invalid' : '' }}" id="{{ $field['name'] }}" placeholder="{{ $field['label'] }}" rows="6" {{ $required }}>
-    @if(isset($field['display']))
-    @if($field['display'] == 'raw')
-    {!! old($field['name'], setting($field['name'])) !!}
-    @else 
-    {{ old($field['name'], setting($field['name'])) }}
-    @endif
-    @else
-    {{ old($field['name'], setting($field['name'])) }}
-    @endif
+    <textarea 
+        type="{{ $field['type'] }}" 
+        name="{{ $field['name'] }}" 
+        class="form-control {{ Arr::get( $field, 'class') }} {{ $errors->has($field['name']) ? ' is-invalid' : '' }}" 
+        id="{{ $field['name'] }}" 
+        placeholder="{{ $field['label'] }}" 
+        rows="6" 
+        {{ $required }}
+    >
+        @if(isset($field['display']))
+        @if($field['display'] == 'raw')
+        {!! old($field['name'], setting($field['name'])) !!}
+        @else 
+        {{ old($field['name'], setting($field['name'])) }}
+        @endif
+        @else
+        {{ old($field['name'], setting($field['name'])) }}
+        @endif
     </textarea>
 
     @if ($errors->has($field['name'])) <small class="invalid-feedback">{{ $errors->first($field['name']) }}</small> @endif
