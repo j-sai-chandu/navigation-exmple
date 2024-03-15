828ba7cf1d5b3c9b15d6af88d5dc6e0ee6c63a7b
Lyove <lyove@outlook.com>
2024-03-16 03:09:39 +0800

    init

--- modules/Category/Resources/lang/en/list.php
+++ modules/Category/Resources/lang/en/list.php
@@ -4,6 +4,7 @@ return [
 
     'id' => 'ID',
     'name' => 'Name',
+    'name' => 'Status',
     'updated_at' => 'Updated At',
     'action' => 'Action',
 
--- modules/Category/Resources/lang/zh_CN/list.php
+++ modules/Category/Resources/lang/zh_CN/list.php
@@ -4,6 +4,7 @@ return [
 
     'id' => 'ID',
     'name' => '名称',
+    'name' => '名称',
     'updated_at' => '更新于',
     'action' => '操作',
 
