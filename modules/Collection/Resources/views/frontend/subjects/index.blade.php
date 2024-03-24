3a16ecad7a5e7bce3a7489d3bbb9490163439b5f
Lyove <lyove@outlook.com>
2024-03-24 22:48:18 +0800

    init

--- modules/Collection/Resources/views/frontend/subjects/index.blade.php
+++ modules/Collection/Resources/views/frontend/subjects/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<section class="bg-gray-100 text-gray-600 py-20">
+<section class="bg-gray-100 text-gray-600 py-10">
     <div class="container mx-auto flex px-8 items-center justify-center flex-col">
         <div class="text-center lg:w-2/3 w-full">
             <h1 class="text-3xl sm:text-4xl mb-4 font-medium text-gray-800">
