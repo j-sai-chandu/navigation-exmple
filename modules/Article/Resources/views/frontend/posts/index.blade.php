92281df78fce5316156736cf0bf266d40c903249
Lyove <lyove@outlook.com>
2024-04-01 18:05:54 +0800

    init

--- modules/Article/Resources/views/frontend/posts/index.blade.php
+++ modules/Article/Resources/views/frontend/posts/index.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 
-<section class="bg-gray-100 text-gray-600 py-20">
+<section class="bg-gray-100 text-gray-600 py-10">
     <div class="container mx-auto flex px-5 items-center justify-center flex-col">
         <div class="text-center lg:w-2/3 w-full">
             <h1 class="text-3xl sm:text-4xl mb-4 font-medium text-gray-800">
                 {{ __($module_title) }}
             </h1>
-            <p class="mb-8 leading-relaxed">
+            <p class="leading-relaxed">
                 The list of {{ __($module_name) }}.
             </p>
 
