c06a6058c83e27e8a3c2ff4163f42f8e6f2f6a52
Lyove <lyove@outlook.com>
2024-04-03 10:52:16 +0800

    init

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,20 @@
+@props(['title', 'desc', 'more_url'])
+
+<div class="w-full mx-auto flex flex-col items-center justify-center border border-gray-200 rounded-md hover:shadow-lg">
+    <div class="w-full relative px-6 py-4 border-b border-gray-100">
+        <h3 class="text-lg leading-6 font-medium text-gray-800">
+            @lang($title)
+        </h3>
+        <p class="max-w-2xl text-sm text-gray-500 mb-0">
+            @lang($desc)
+        </p>
+        @if ($more_url)
+        <span class="flex items-center h-full absolute right-0 top-0 bottom-0 px-3">
+            <a href="{$more_url}" target="_blank">@lang("More")</a>
+        </span>
+        @endif
+    </div>
+    <div class="w-full py-3">
+        {!! $slot !!}
+    </div>
+</div>
