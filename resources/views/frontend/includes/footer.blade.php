fa45c25dca33eee4bf32e5a0f779fd5392cd70cc
Lyove <lyove@outlook.com>
2024-04-09 17:55:59 +0800

    init

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,15 +1,19 @@
+@php
+$route_path = Request::route()->getName();
+@endphp
+
 <footer class="text-center bg-gray-200">
     <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
         <div class="mx-auto space-y-6">
             <nav class="relative flex flex-wrap justify-center gap-8 p-4 text-sm font-bold">
-                <a href="{{ route('frontend.categories.index') }}" class="text-gray-600 border-transparent border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
-                    {{__('Categories')}}
+                <a href="{{ route('frontend.categories.index') }}" class="{{ $route_path == 'frontend.categories.index' ? 'active border-orange-600' : 'border-transparent'}} text-gray-600 border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
+                    @lang('Categories')
                 </a>
-                <a href="{{ route('frontend.tags.index') }}" class="text-gray-600 border-transparent border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
-                    {{__('Tags')}}
+                <a href="{{ route('frontend.tags.index') }}" class="{{ $route_path == 'frontend.tags.index' ? 'active border-orange-600' : 'border-transparent'}} text-gray-600 border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
+                    @lang('Tags')
                 </a>
-                <a href="{{ route('frontend.taxons.index') }}" class="text-gray-600 border-transparent border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
-                    {{__('Taxons')}}
+                <a href="{{ route('frontend.taxons.index') }}" class="{{ $route_path == 'frontend.taxons.index' ? 'active border-orange-600' : 'border-transparent'}} text-gray-600 border-b-2 hover:border-orange-600 px-3 py-2 text-base font-medium transition ease-out duration-300">
+                    @lang('Taxons')
                 </a>
             </nav>
             <p class="text-xs font-medium">
