efa76b4feace926248044adfc2856065bbccad00
Lyove <lyove@outlook.com>
2024-04-09 16:00:23 +0800

    init

---
+++ modules/Article/Resources/assets/js/backend.js
@@ -0,0 +1,3 @@
+import "../../../modules/Page/Resources/assets/js/backend.js";
+import "../../../modules/Article/Resources/assets/js/backend.js";
+import "../../../modules/Collection/Resources/assets/js/backend.js";
---
+++ modules/Article/Resources/assets/js/frontend.js
@@ -0,0 +1,3 @@
+import "../../../modules/Page/Resources/assets/js/frontend.js";
+import "../../../modules/Article/Resources/assets/js/frontend.js";
+import "../../../modules/Collection/Resources/assets/js/frontend.js";
